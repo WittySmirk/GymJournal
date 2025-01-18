5f092ff60835960bc2775df5b20c15e4063606b7
WittySmirk <wyattdegenhart@gmail.com>
2025-01-18 13:51:06 -0700

    modularized the modal component

--- frontend/src/components/exerciseButton.tsx
+++ frontend/src/components/exerciseButton.tsx
@@ -1,39 +1,101 @@
 // TODO: Create the modals
 import "../App.css";
 import { useState } from 'react';
-import Modal from "../components/modal.tsx"
+import Modal from ".//modal";
 
 function ExerciseButton(props: { create: boolean, name?: string, id?: string, fetchApi?: VoidFunction }) {
   const [modal, setModal] = useState<boolean>(false);
-  /*
-  async function postToServer() {
+  async function modalAction(formData: any) {
+    if (!props.create) {
+
+      const weight = Number(formData.get("weight"));
+      const sets = Number(formData.get("sets"));
+      const reps = Number(formData.get("reps"));
+
+      if (isNaN(weight) || isNaN(sets) || isNaN(reps)) {
+        // TODO: Set up some type of error thing here that says must be a number
+        setModal(false);
+        return;
+      }
+
+      const d = {
+        create: false,
+        id: props.id,
+        weight: weight,
+        sets: sets,
+        reps: reps,
+      };
+
+      await fetch("http://localhost:8080/app", {
+        method: "POST",
+        body: JSON.stringify(d),
+        credentials: "include"
+      });
+
+      setModal(false);
+      props.fetchApi!();
+      return;
+    }
+
+    const name = String(formData.get("name"));
+    if (!(typeof name === "string")) {
+      // TODO: Set up some type of error thing here that says must be a number
+      setModal(false);
+      return;
+    }
+
     const d = {
-      create: props.create,
-      id: props.id,
-      message: "hello buddy"
+      create: true,
+      name: name,
     };
-    // Not sure if we want to get the response or not
     await fetch("http://localhost:8080/app", {
       method: "POST",
-      body: JSON.stringify(d)
-    })
-  }
-  */
+      body: JSON.stringify(d),
+      credentials: "include"
+    });
 
-  // TODO: Figure out this type??
+    setModal(false);
+    props.fetchApi!();
+  }
 
   return (
     <>
-      <button onClick={() => setModal(true)} className="exerciseButton">
+      <span onClick={() => setModal(true)} className="exerciseButton">
         {props.create ? "Create New" : props.name}
-      </button>
+      </span>
 
       {modal ?
-        <Modal create={props.create} id={props.id} name={props.name} resetModal={() => {
-          setModal(false)
-          props.fetchApi!()
-        }} />
-        : <></>}
+        <Modal formAction={modalAction} id={props.id} name={props.name}>
+          {props.create ? (
+            <>
+              <h1>Create Exercise</h1>
+              <label className="modal-item" htmlFor="name">Exercise Name</label>
+              <input className="modal-item" type="text" id="name" name="name" required />
+              <div className="modal-item">
+                <input type="submit" value="submit" />
+                <button type="button" onClick={() => setModal(false)}>Close</button>
+              </div>
+            </>
+          ) : (
+            <>
+              <h1>{props.name}</h1>
+              <label className="modal-item" htmlFor="weight">Weight</label>
+              <input className="modal-item" type="text" id="weight" name="weight" required />
+              <label className="modal-item" htmlFor="sets">Sets</label>
+              <input className="modal-item" type="text" id="sets" name="sets" required />
+              <label className="modal-item" htmlFor="reps">Reps</label>
+              <input className="modal-item" type="text" id="reps" name="reps" required />
+              <div className="modal-item">
+                <input type="submit" value="submit" />
+                <button type="button" onClick={() => setModal(false)}>Close</button>
+              </div>
+
+            </>
+          )}
+
+        </Modal >
+        : <></>
+      }
     </>
   );
 }
--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import ExerciseButton from "../components/exerciseButton";
+import Modal from "../components/modal";
 import '../App.css'
 
 /*
@@ -17,12 +18,14 @@ type Exercise = {
 };
 
 type Data = {
+  nameNotValid: boolean,
   name: string,
   exercises: Exercise[]
 };
 
 function App() {
   const [data, setData] = useState<Data | undefined>(undefined);
+  const [nameNotValid, setNameNotValid] = useState<boolean>(false);
 
   async function fetchApi() {
     // TODO: Figure out environment variables
@@ -32,7 +35,32 @@ function App() {
 
 
     const json = await raw.json();
+    console.log(json);
     setData(json);
+    if (data!.nameNotValid) {
+      setNameNotValid(true);
+    }
+  }
+  async function nameAction(formData: any) {
+    const name = String(formData.get("name"));
+    if (!(typeof name === "string")) {
+      // TODO: set up error handling
+      setNameNotValid(false);
+      return;
+    }
+    const d = {
+      createName: true,
+      name: name,
+    };
+
+    await fetch("http://localhost:8080/app", {
+      method: "POST",
+      body: JSON.stringify(d),
+      credentials: "include"
+    });
+
+    setNameNotValid(false);
+    fetchApi();
   }
 
   useEffect(() => {
@@ -41,6 +69,20 @@ function App() {
 
   return (
     <>
+      {nameNotValid ? (
+        <>
+          <Modal formAction={nameAction} >
+            <h1>Create User Name</h1>
+            <label className="modal-item" htmlFor="name">Please Enter Your Name</label>
+            <input className="modal-item" type="text" id="name" name="name" required />
+            <div className="modal-item">
+              <input type="submit" value="submit" />
+            </div>
+          </Modal>
+        </>
+      ) : (
+        <>
+        </>)}
       {data ? (
         <>
           <div className="navBar">
